678417ee5aac0947c2a5c38826da878ccc017d05
Atsushi Takayama <taka.atsushi@gmail.com>
2019-03-14 00:10:11 +0900

    Optimize

--- ALDS1_1_D/a.cpp
+++ ALDS1_1_D/a.cpp
@@ -1,40 +1,24 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-  vector<int> nums;
+  int diff = -1e9-1;
 
-  string tmp;
+  int minv;
+  cin >> minv; // drop 1st line
+  cin >> minv;
 
-  getline(cin, tmp); // drop 1st line
-
-  while (getline(cin, tmp)) {
-    stringstream ss(tmp);
-    int ti;
-    ss >> ti;
-    nums.push_back(ti);
-  }
-
-  int minv = nums[0];
-  int diff = nums[1] - nums[0];
-
-  //for(int n: nums){
-  for(int i = 1; i < (int)nums.size(); ++i) {
-    int n = nums[i];
+  int n;
+  while (cin >> n) {
     if (diff < n - minv) {
-      //cout << n << " " << minv << endl;
       diff = n - minv;
     }
     if (n < minv) {
-      //cout << n << endl;
       minv = n;
     }
   }
-
   cout << diff << endl;
   return 0;
 }
