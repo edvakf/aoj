140d9725be11c32af22e22aafd87ed224d9e5c03
Atsushi Takayama <taka.atsushi@gmail.com>
2019-04-23 00:48:33 +0900

    Extract bubbleSort into a function

--- ALDS1_2_A/main.cpp
+++ ALDS1_2_A/main.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <iterator>  // std::ostream_iterator
-#include <algorithm> // std::copy
-#include <sstream>   // std::stringstream
 
 using namespace std;
 
-void output(vector<int> nums) {
-  copy(nums.begin(), nums.end()-1, ostream_iterator<int>(cout, " "));
-  cout << nums.back() << endl;
-}
-
-int main(int argc, char** argv) {
-  vector<int> nums;
-
-  string tmp;
-
-  getline(cin, tmp); // drop 1st line
-  getline(cin, tmp); // 2nd line
-
-  stringstream ss(tmp);
-  int ti;
-  while (ss >> ti) {
-    nums.push_back(ti);
-  }
-
-  // output(nums);
-
+int bubbleSort(vector<int> &nums) {
   int swapcount = 0;
 
   bool flag = true;
@@ -45,7 +22,28 @@ int main(int argc, char** argv) {
     }
   }
 
-  output(nums);
+  return swapcount;
+}
+
+int main(int argc, char** argv) {
+  vector<int> nums;
+
+  size_t N;
+  cin >> N;
+
+  for (size_t i = 0; i < N; i++) {
+    int ti;
+    cin >> ti;
+    nums.push_back(ti);
+  }
+
+  int swapcount = bubbleSort(nums);
+
+  for (size_t i = 0; i < N; i++) {
+    if (i > 0) cout << " ";
+    cout << nums[i];
+  }
+  cout << endl;
   cout << swapcount << endl;
 
   return 0;
