d474740b08d4ed4891b3d0e8cba898c3ac8e4bf2
Atsushi Takayama <taka.atsushi@gmail.com>
2019-08-04 14:29:39 +0900

    Add ALDS1_8_B

---
+++ ALDS1_8_B/main.cpp
@@ -0,0 +1,99 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+  int key;
+  Node *right, *left, *parent;
+};
+
+Node *root, *NIL;
+
+void insert(int k) {
+  Node *y = NIL;
+  Node *x = root;
+  Node *z;
+
+  z = (Node *)malloc(sizeof(Node));
+  z->key = k;
+  z->left = NIL;
+  z->right = NIL;
+
+  while (x != NIL) {
+    y = x;
+    if (z->key < x->key) {
+      x = x->left;
+    } else {
+      x = x->right;
+    }
+  }
+  z->parent = y;
+  if (y == NIL) {
+    root = z;
+  } else {
+    if (z->key < y->key) {
+      y->left = z;
+    } else {
+      y->right = z;
+    }
+  }
+}
+
+bool find(int k) {
+  Node *x = root;
+  while (x != NIL) {
+    if (k == x->key) {
+      return true;
+    } else if (k < x->key) {
+      x = x->left;
+    } else {
+      x = x->right;
+    }
+  }
+  return false;
+}
+
+void inorder(Node *u) {
+  if (u == NIL) return;
+  inorder(u->left);
+  printf(" %d", u->key);
+  inorder(u->right);
+}
+void preorder(Node *u) {
+  if (u == NIL) return;
+  printf(" %d", u->key);
+  preorder(u->left);
+  preorder(u->right);
+}
+
+int main() {
+  int n, i, x;
+  string com;
+
+  scanf("%d", &n);
+
+  for (i = 0; i < n; i++) {
+    cin >> com;
+    if (com == "insert") {
+      scanf("%d", &x);
+      insert(x);
+    } else if (com == "find") {
+      scanf("%d", &x);
+      if (find(x)) {
+        printf("yes\n");
+      } else {
+        printf("no\n");
+      }
+    } else if (com == "print") {
+      inorder(root);
+      printf("\n");
+      preorder(root);
+      printf("\n");
+    }
+  }
+
+  return 0;
+}
